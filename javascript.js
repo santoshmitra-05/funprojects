a6a34c0edd28f55d6b5e23d4d47d8a3a2c3a2494
santoshmitra-05 <122418085+santoshmitra-05@users.noreply.github.com>
2023-03-08 00:41:14 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,52 @@
+/*window.alert("This page is unresponsive");
+let age = 24
+console.log(age);
+students = 55
+x = students%3
+console.log(x);
+x1 = "4.123"
+console.log(typeof x1)
+x2 = true
+console.log(typeof x2)
+let pi = 3.14
+let radius = window.prompt("Enter the radius of a circle");
+let circum = 2*pi*radius;
+window.alert("The circumference is"+" "+circum);*/
+// let x = 4.12
+// let c = Math.round(x)
+// console.log(c);
+// let a;
+// let b;
+// let c;
+// a = window.prompt("Enter the base of the triangle")
+// b = window.prompt("Enter the value of the height of the triangle");
+// c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2))
+// console.log("The hypotenuse of the triangle is"+" "+c)
+// let x = (Math.random()*6)+1
+// console.log(x);
+// document.getElementById("roll").innerHTML 
+// let username = window.prompt("Enter the username");
+// console.log("The length of your name is"+" "+username.length)
+// x = username.toUpperCase();
+// console.log("Your name in uppercase is"+" "+x)
+// let username = "Santosh Mitra"
+// let partname = username.charAt(4);
+// console.log(partname)
+/*let age = window.prompt("Enter the user's age")
+if(age<=18)
+{
+    window.alert("You are just about to be eligible to vote");
+}
+if(age>18)
+{
+    window.alert("You are not a child any longer. Go to vote");
+}*/
+let online = true;
+if(online)
+{
+    console.log("Your are online");
+}
+else
+{
+    console.log("You are offline");
+}
